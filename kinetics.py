0bb182d9e0c0c72fe53a8abdfdc571c6600c36d9
unknown <rc688@cam.ac.uk>
2018-10-21 02:20:38 +0100

    more or less working, maybe takes too long a time though

--- kinetics.py
+++ kinetics.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import os
 import time
 
-h = 10e-6
+h = 10e-7
 
 
 class Rxn:
@@ -17,13 +17,11 @@ class Rxn:
         self.reag = reag
         self.prod = prod
 
-
     def isReag(self, rg):
         return rg in self.reag
 
-    def isProd(self, rg):
-        return rg in self.prod
-
+    def nProd(self, rg):
+        return self.prod.count(rg)
 
 
 def readFile(file):
@@ -57,16 +55,24 @@ def readFile(file):
     return Reags, Rxns
 
 
-def next(Reags, Rxns):  # will be called steps times
+def reactions(Reags, Rxns):
+    """Given Reags and Rxns forms diff eq forms for each reagent
+    """
     reactions = {}
     for r in Reags:
         reactions[r] = []
         for rxn in Rxns:
             if rxn.isReag(r):
                 reactions[r].append((-rxn.rate, rxn.reag))
-            elif rxn.isProd(r):
-                reactions[r].append((rxn.rate, rxn.reag))
-    # now we have a list  of reactions to work with
+            a = rxn.nProd(r)
+            if a:
+                reactions[r].append((a*rxn.rate, rxn.reag))
+    return reactions
+
+
+def next_euler(Reags, reactions):  # will be called steps times
+    """
+    """
     for diff in reactions:
         delta = 0
         for i in reactions[diff]:
@@ -74,24 +80,51 @@ def next(Reags, Rxns):  # will be called steps times
             reags = np.array([Reags[i] for i in reags])
             delta = delta + rate*np.prod(reags)
         Reags[diff] = Reags[diff] + h*delta
-    # print([Reags[i] for i in Reags])
-    return Reags, Rxns
+    return Reags
 
 
-def rk4(f, y, x):
-    a = f(y, x)
-    b = f(y, x + h/2*a)
-    c = f(y, x + h/2*b)
-    d = f(y, x + h*c)
-    return x + h/6*(a+2*b+2*c+d)
+def next_rk4(Reags, reactions):  # will be called steps times
+    a, b,  c = {}, {}, {}
+    for diff in reactions:
+        j = 0
+        for i in reactions[diff]:
+            rate, reags = i
+            reags = np.array([Reags[i] for i in reags])
+            j = j + rate*np.prod(reags)
+        a[diff] = j
+    for diff in reactions:
+        j = 0
+        for i in reactions[diff]:
+            rate, reags = i
+            reags = np.array([Reags[i] + 0.5*h*a[i] for i in reags])
+            j = j + rate*np.prod(reags)
+        b[diff] = j
+    for diff in reactions:
+        j = 0
+        for i in reactions[diff]:
+            rate, reags = i
+            reags = np.array([Reags[i] + 0.5*h*b[i] for i in reags])
+            j = j + rate*np.prod(reags)
+        c[diff] = j
+    for diff in reactions:
+        j = 0
+        for i in reactions[diff]:
+            rate, reags = i
+            reags = np.array([Reags[i] + h*c[i] for i in reags])
+            j = j + rate*np.prod(reags)
+#            if j < 1: print(j)
+        Reags[diff] = Reags[diff] + h*(a[diff]+2*b[diff]+2*c[diff]+j)/6
+
+    return Reags
 
 
 def euler(f, y, x):
-    a = f(y, x+h)
+    a = f(y, x)
     return x + h*a
 
 
-def run(Reags, Rxns, steps):
+def run(Reags, Rxns, steps, plot=True):
+    reacts = reactions(Reags, Rxns)
     points, i = [], 0
     for r in Reags:
         points.append([])
@@ -101,15 +134,19 @@ def run(Reags, Rxns, steps):
             points[i].append(Reags[r])
             i = i + 1
         i = 0
-        Reags, Rxns = next(Reags, Rxns)
-
-    ax = plt.axes()
-    for r in Reags:
-        ax.plot(points[i])
-        i = i+1
-    # ax.set_yscale('log')
-    ax.set_xlabel("time / s")
-    ax.set_ylabel("concentration / M")
+        Reags = next_euler(Reags, reacts)
+    if plot:
+        ax = plt.axes()
+        ax.plot([x*h for x in range(steps)], points[4])
+        ax.plot([x*h for x in range(steps)], points[5])
+        ax.plot([x*h for x in range(steps)], points[6])
+#        a = np.array(points[0])
+#        b = np.array(points[1])
+#        c = np.array(points[2])
+#        ax.plot(a+b+c)
+        ax.set_yscale('log')
+        ax.set_xlabel("time / s")
+        ax.set_ylabel("concentration / M")
     i = 0
     ret = [0, 0, 0]
     for r in points:
@@ -119,36 +156,20 @@ def run(Reags, Rxns, steps):
     return ret
 
 
-
 def main():
     t1 = time.time()
-#    Reags, Rxns = readFile("reaction.txt")
-#    print(run(Reags, Rxns, 100000))
-    plot2 = []
-    for file in sorted(os.listdir("./protein_input/")):
-        Reags, Rxns = readFile("./protein_input/"+file)
-        plot2.append(run(Reags, Rxns, 100000))
-    plt.plot(plot2)
+    Reags, Rxns = readFile("oregonator.txt")
+    print(run(Reags, Rxns, 100000000))
+#    urea = []
+#    plot2 = []
+#    for file in os.listdir("./protein_input/"):
+#        urea.append(float(file.split('.txt')[0][7:]))
+#        Reags, Rxns = readFile("./protein_input/"+file)
+#        plot2.append(run(Reags, Rxns, 10000))
+#    plt.plot(urea, plot2)
     t2 = time.time()
     print(t2-t1)
 
+
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
