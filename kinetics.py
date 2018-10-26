78829f69769d840ef388c8316d5f0709da5a6a8c
unknown <rc688@cam.ac.uk>
2018-10-26 01:03:24 +0100

    Final version of the kinetics module

--- kinetics.py
+++ kinetics.py
@@ -1,20 +1,44 @@
-"""To be added
+"""Python module for kinetic reaction simulation
 
+Supports text file input of elementar reactions and initial conditions
+Example input:
+    A=5,B=0,C=2,D=0
+    A,B|C|10
+    C|D|0.5
+where first line specifies starting concentrations and subsequent
+lines define elementary reactions, so that reaction
+A + B -> C with rate 10 becomes A,B|C|10
+
+Method supported:
+    readFile(file): parses the input file
+    reactions(Reags, Rxns): parses the output of readFile
+    run(Reags, Rxns, steps, nspace=1): runs the simulation
+    next_euler(Reags, reacts, rlist): calculates concentrations after timestep
 """
-import numpy as np
+
+
 import matplotlib.pyplot as plt
 import os
 import time
 from functools import reduce
 
-h = 1e-6
-steps = 1000000
-defFile = "oregonator.txt"
-nspace = 10
+# default values for simulation steps
+h = 1e-3  # simulation timestep
+steps = 10000
+nspace = 10  # after how many steps to save concentrations for plot
 
 
 class Rxn:
-    """What am I doing??
+    """Class that describes each elementary reaction
+
+    Initialised with:
+        rate - float defining reaction rate
+        reag - list of names of reagents in reaction
+        prod - list of names of products in reaction
+
+    Contains two functions:
+        nReag(string r) returns the order of r in reaction
+        nProd(string r) : returns number of molecules of r produced in reaction
     """
     def __init__(self, rate, reag, prod):
         self.rate = rate
@@ -61,6 +85,18 @@ def readFile(file):
 
 def reactions(Reags, Rxns):
     """Given Reags and Rxns forms diff eq forms for each reagent
+
+    Returns dictionary of diff eq's in form:
+        {Reagent1: [(rate const wrt reagent, index of reaction),
+                     ...]],
+         Reagent2: ...,
+         ...}
+    and list of reactions as indexed in the dictionary:
+        [[reag1, reag2], ...]
+
+    intended use is to replace the reags by name with their concentrations,
+    then multiply together and with rate corected for each reagent to yield
+    the respective change
     """
     reactions_dict = {}
     reactions_list = []
@@ -89,7 +125,16 @@ def reactions(Reags, Rxns):
     return (reactions_dict, reactions_list)
 
 
-def next_euler(Reags, reacts, rlist):  # will be called steps times
+def next_euler(Reags, reacts, rlist):
+    """Calls the next time step
+
+    As tested with the Oregonator, the small concentration changes are very
+    importent, so small time steps are required, therefore when tested with
+    Runge-Kutta 4th order integrator the performance was worse.
+
+    Takes in dictionary of reagents Reags and arguments reacts, rlist
+    prepared by reactions method.
+    """
     vals = [reduce(
             lambda x, y: x*y, [Reags[i] for i in reags]) for reags in rlist]
 
@@ -98,10 +143,13 @@ def next_euler(Reags, reacts, rlist):  # will be called steps times
         for rate, indx in reacts[r]:
             delta = delta + rate*vals[indx]
         Reags[r] = Reags[r] + h*delta
-    return 0
+    return
 
 
 def run(Reags, Rxns, steps, nspace=1):
+    """Simulates the system for specified steps and returns concentrations
+    at each specified time.
+    """
     (reacts, rlist) = reactions(Reags, Rxns)
     points, i = [], 0
     for r in Reags:
@@ -118,23 +166,24 @@ def run(Reags, Rxns, steps, nspace=1):
     return points
 
 
+# used for testing the kinetic model used
 def main():
     t1 = time.time()
-    Reags, Rxns = readFile("oregonator.txt")
+    Reags, Rxns = readFile("reaction.txt")
     points = run(Reags, Rxns, steps, nspace)
+
     ax = plt.axes()
-    ax.plot([nspace*x*h for x in range(int(steps/nspace))], points[4],
-            label='X')
-    ax.plot([nspace*x*h for x in range(int(steps/nspace))], points[5],
-            label='Y')
-    ax.plot([nspace*x*h for x in range(int(steps/nspace))], points[6],
-            label='Z')
+    i = 0
+    for r in Reags:
+        ax.plot([nspace*x*h for x in range(int(steps/nspace))], points[i],
+                label=r)
+        i = i + 1
+
     ax.legend()
-    ax.set_yscale('log')
+#    ax.set_yscale('log')
     ax.set_xlabel("time / s")
     ax.set_ylabel("concentration / M")
-    plt.savefig("ayy.png")
-    ax.plot()
+    plt.show()
     t2 = time.time()
     print(t2-t1)
 
