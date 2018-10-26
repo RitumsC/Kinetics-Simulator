e7970b021aa80311ad866459a5fa5e4d6088b8bc
unknown <rc688@cam.ac.uk>
2018-10-26 01:05:21 +0100

    Script to generate required Oregonator output

--- generateOregonator.py
+++ generateOregonator.py
@@ -6,14 +6,20 @@ import time
 import kinetics as kin
 import matplotlib.pyplot as plt
 
+
+# changing the default constants for the specific job
 nspace = 1000
 steps = 45000000
 kin.h = 20e-7
 
 
 t1 = time.time()
+
 Reags, Rxns = kin.readFile("oregonator.txt")
+
 points = kin.run(Reags, Rxns, steps, nspace)
+
+# plot the results
 ax = plt.axes()
 ax.plot([nspace*x*kin.h for x in range(int(steps/nspace))], points[4],
         label='X')
@@ -26,7 +32,8 @@ ax.set_title('Oregonator relevant species concentrations with time')
 ax.set_yscale('log')
 ax.set_xlabel("time / s")
 ax.set_ylabel("concentration / M")
-plt.savefig("test.png")
-ax.plot()
+plt.savefig("oregonator.png")
+plt.show()
+
 t2 = time.time()
-print(t2-t1)
+print("Time taken for calculations: ", t2-t1, " seconds")
